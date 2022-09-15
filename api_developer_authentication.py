e7adcadd5cd943a59ee996bc53541a025c465713
Manoj Rudraraju <manojrudraraju007@gmail.com>
2022-09-15 09:45:52 -0500

    Update api_developer_authentication.py

--- api_developer_authentication.py
+++ api_developer_authentication.py
@@ -1,11 +1,11 @@
 class api_developer_authentication:
 
 	def __init__(self):
-		self.consumer_key =""
-		self.consumer_secret=""
-		self.access_token=""
-		self.access_token_secret=""
-		self.bearer_token = ""
+		self.consumer_key ="sXJWjPL5odLBFBUXRQfZKEO1O"
+		self.consumer_secret="362LzIMRR0mLLyBSBEqlgsZuJOUMvTmJfvv6tqVSMcLe1pSAOl"
+		self.access_token="1514751578636443649-XsiQ9KKiaHbCrpYjJvvFPj7YaC3R3l"
+		self.access_token_secret="HV0vZYGmNQ7K9SCq0x5nG4XyhuUxdzcO6VqWQnrVZzEPp"
+		self.bearer_token = "AAAAAAAAAAAAAAAAAAAAABIFbgEAAAAAphfyZypdNOh%2FwA%2FFYLC%2FgTuQEy0%3DgOV3M7cpycYn0XfbTnCwQETbW9ZQzzSOwY3x88EqtUbi2ODncP"
 		self.iteration = 2
 		self.time_interval = 10 # value in second
 		
