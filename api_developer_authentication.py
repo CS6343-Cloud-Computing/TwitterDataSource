a6a801dfc020ac2ff4aba8be108b7aa69accc8dc
manojvarmarudraraju <manojrudraraju007@gmail.com>
2022-09-15 10:03:24 -0500

    api key changes

--- api_developer_authentication.py
+++ api_developer_authentication.py
@@ -1,14 +1,14 @@
 class api_developer_authentication:
 
 	def __init__(self):
-		self.consumer_key ="sXJWjPL5odLBFBUXRQfZKEO1O"
-		self.consumer_secret="362LzIMRR0mLLyBSBEqlgsZuJOUMvTmJfvv6tqVSMcLe1pSAOl"
-		self.access_token="1514751578636443649-XsiQ9KKiaHbCrpYjJvvFPj7YaC3R3l"
-		self.access_token_secret="HV0vZYGmNQ7K9SCq0x5nG4XyhuUxdzcO6VqWQnrVZzEPp"
-		self.bearer_token = "AAAAAAAAAAAAAAAAAAAAABIFbgEAAAAAphfyZypdNOh%2FwA%2FFYLC%2FgTuQEy0%3DgOV3M7cpycYn0XfbTnCwQETbW9ZQzzSOwY3x88EqtUbi2ODncP"
+		self.consumer_key ="czhHNERMa2hLRTk1VUp3RnRRTEM6MTpjaQ"
+		self.consumer_secret="GJUhfm4RrDx3Jsaw6RbW3ud5CdIXTD4e_fiOXW7XyAbR9qE6gt"
+		self.access_token="1514751578636443649-NzpDCEbuM67LNTsKUDQaV7uNNKTv74"
+		self.access_token_secret="LziI1QVvfqZYEmqA8qtdA9hutuq8uwCbEEguaOuRzhsr5"
+		self.bearer_token = "AAAAAAAAAAAAAAAAAAAAABIFbgEAAAAA86RFHhOkSuTysFQDLk1M9qnv8zI%3DKAqlTggrWW4W3fsDMgqqUMKCNlGisAwT2wru0bHkgsIW3wTE6B"
 		self.iteration = 2
 		self.time_interval = 10 # value in second
-		
+
 	def consumer_key_val(self):
 		return self.consumer_key
 	def consumer_secret_val(self):
