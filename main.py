6693d2d4b0ab4e77b3bb02d094e8b9aa2161b9bc
m1-vm2 <cs6343g7>
2022-11-12 20:12:20 +0000

    final changes

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ from api_developer_authentication import api_developer_authentication
 import os
 import DBConnect
 
-
-KafkaServer = os.environ.get('KafkaServer')
-containerName = os.environ.get('ContainerName')
+KafkaServer = sys.argv[1]
 
 auth_kay_val = api_developer_authentication()
 consumer_key = auth_kay_val.consumer_key_val()
@@ -23,11 +21,6 @@ producer = KafkaProducer(bootstrap_servers=[KafkaServer],api_version=(0,11,5))
 MAX_CALL = 1
 polling_interval = auth_kay_val.polling_time_interval()
 
-consumer = KafkaConsumer(
- bootstrap_servers=KafkaServer, api_version=(0,11,5), auto_offset_reset='earliest', group_id=None
-)
-consumer.assign([TopicPartition(containerName, 0)])
-
 client = tweepy.Client(bearer_token)
 counter = 0
 lastTweet = 0
@@ -36,38 +29,26 @@ call_controller = 0
 
 
 while True:
-	hashs = DBConnect.getHashTags()
-	print(hashs)
-	for hash in hashs:
-		query = hashs[hash]["query"]
-		call_controller = 0
-		lastTweet = 0
-		headerSteps = hashs[hash]["steps"]
-		pointer = hashs[hash]["pointer"] + 1
-		while(call_controller < MAX_CALL):
-			counter = 0
-			call_controller += 1
-			if lastTweet == 0:
-				for tweet in tweepy.Paginator(client.search_recent_tweets, query=query, max_results = 10).flatten(limit=200):
-					print(tweet)
-					if(counter == 0):
-						lastTweet = tweet.id
-						counter+=1
-					else:
-						tweet_id = str(tweet.id)
-						print("=======> ", headerSteps[pointer])
-						producer.send(headerSteps[pointer],  key = tweet_id.encode('utf-8'), value = str(tweet.text).encode('utf-8'),partition=0, headers=[('flow', "<-->".join(headerSteps).encode()), ('pointer', str(1).encode())])
-						#break
-			else:
-				for tweet in tweepy.Paginator(client.search_recent_tweets,since_id = lastTweet, query=query, max_results = 10).flatten(limit = 200):
-					if(counter == 0):
-						lastTweet = tweet.id
-						counter+=1
-					else:
-						tweet_id = str(tweet.id)
-						print("=======> ", headerSteps[pointer])
-						producer.send(headerSteps[pointer],  key = tweet_id.encode('utf-8'), value = str(tweet.text).encode('utf-8'),partition=0, headers=[('flow', "<-->".join(headerSteps).encode()), ('pointer', str(1).encode())])
-						#break
-			producer.flush()
+	while(call_controller < MAX_CALL):
+		counter = 0
+		call_controller += 1
+		if lastTweet == 0:
+			for tweet in tweepy.Paginator(client.search_recent_tweets, query=query, max_results = 10).flatten(limit=200):
+				print(tweet)
+				if(counter == 0):
+					lastTweet = tweet.id
+					counter+=1
+				else:
+					tweet_id = str(tweet.id)
+					producer.send("wfm-input",  key = tweet_id.encode('utf-8'), value = str(tweet.text).encode('utf-8'),partition=0, headers=[('workflow',int(sys.argv[2]).encode())])
+		else:
+			for tweet in tweepy.Paginator(client.search_recent_tweets,since_id = lastTweet, query=query, max_results = 10).flatten(limit = 200):
+				if(counter == 0):
+					lastTweet = tweet.id
+					counter+=1
+				else:
+					tweet_id = str(tweet.id)
+					producer.send("wfm-input",  key = tweet_id.encode('utf-8'), value = str(tweet.text).encode('utf-8'),partition=0, headers=[('workflow',int(sys.argv[2]).encode())])
+		producer.flush()
 	time.sleep(1)
 			# time.sleep(polling_interval)
